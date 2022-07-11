21d1b09c9e1af4c8367a65d29b7a4cd0544e62be
MunyDev <66443942+MunyDev@users.noreply.github.com>
2022-07-10 21:42:06 -0400

    Revert "Tj"

--- teavm/src/main/java/org/lwjgl/opengl/BridgeConstants.java
+++ teavm/src/main/java/org/lwjgl/opengl/BridgeConstants.java
@@ -3,10 +3,13 @@ package org.lwjgl.opengl;
 import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.BiConsumer;
 
 import org.teavm.webgl2.WebGL2RenderingContext;
 
+import static org.lwjgl.opengl.RealOGLConstants.*;
 
+import sun.misc.Unsafe;
 /**
  * This is a massive class mapping real constants to webgl constants
  * @author MunyDev
@@ -573,7 +576,7 @@ public class BridgeConstants {
 		// It would take me then hours to store them in a file and loop'em in the system.
 		Class<WebGLConstants> c = WebGLConstants.class;
 		for (Field f : c.getFields()) {
-			if (f.getType().getName().equals("int")) {
+			if (f.getType().getName() == "int") {
 				try {
 					int i = (Integer)f.get(null);
 					System.out.println(f.getName());
@@ -587,7 +590,6 @@ public class BridgeConstants {
 				}
 			}
 		}
-
 		ogltowgl.put("GL_DEPTH_BUFFER_BIT","DEPTH_BUFFER_BIT");
 		ogltowgl.put("GL_STENCIL_BUFFER_BIT","STENCIL_BUFFER_BIT");
 		ogltowgl.put("GL_COLOR_BUFFER_BIT","COLOR_BUFFER_BIT");
@@ -889,18 +891,21 @@ public class BridgeConstants {
 	}
 	public static void readOverClass(Class<?> cl) {
 		for (Field f : cl.getFields()) {
-			if (f.getType().getName().equals("int")){
+			if (f.getType().getName() == "int") {
 				try {
 					int t = (int) f.get(null);
 					String s = f.getName();
 					
 					m.put(s, t);
-				} catch (IllegalArgumentException | IllegalAccessException e)  {
+				} catch (IllegalArgumentException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IllegalAccessException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
-		}
+		};
 	}
 	public static void readOverClass(Class<?> cl, Map<String, String> ogltowgl) {
 		for (Field f : cl.getFields()) {
@@ -909,5 +914,5 @@ public class BridgeConstants {
 				ogltowgl.put(s, "GL_"+ s);
 			}
 		}
-	}
+	};
 }
